b004b31d776e51cfede02364a6d8f578b65c0bde
lydiacox <coxlydia01@gmail.com>
2025-03-11 22:10:11 +1100

    fmt

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 module "compute_micro_debian_12" {
   source = "./modules/compute_instance"
 
-  project = var.project
-  name = "${var.name}-debian-12"
-  image = "projects/debian-cloud/global/images/family/debian-12"
-  size = var.size
-  type = var.type
+  project      = var.project
+  name         = "${var.name}-debian-12"
+  image        = "projects/debian-cloud/global/images/family/debian-12"
+  size         = var.size
+  type         = var.type
   machine_type = "e2-micro"
-  zone = var.zone
-  subnetwork = google_compute_subnetwork.self.id
+  zone         = var.zone
+  subnetwork   = google_compute_subnetwork.self.id
 }
